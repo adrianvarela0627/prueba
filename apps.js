5c9ccca35d8b7fb8ee903a919c46081e578178c9
AdrianVarelaVeliz <adrianvarela0627@gmail.com>
2022-03-30 11:44:19 -0500

    expresiones js

--- apps.js
+++ apps.js
@@ -1,2 +1,14 @@
-// tipos de datos
-">Hellow Word" // string
+// expresiones! Si podemos ponerlo dentro de un console.log, es una expresión.
+console.log(1 + 2);
+console.log(Math.pow(2,3) + 22);
+
+var numero = 5;
+//console.log((numero > 10 ? 'mayor' : 'menor'));
+
+
+// hace algo
+if (10 > 10) {
+    console.log('Soy mayor')
+  } else {
+    console.log('soy menor')
+  }
